c53af2adb1e29f70a6438f0148b7c24983dfda0b
patrikszepesi <patrikszepesi@gmail.com>
2022-03-09 10:03:17 +0100

    latest

--- ArrayAndStrings/Python/ReverseInplace.py
+++ ArrayAndStrings/Python/ReverseInplace.py
@@ -12,6 +12,6 @@ def reverse(list_of_chars):
 
 
     while left_index<right_index:
-        list_of_chars[left_index],list_of_cahrs[right_index]=list_of_chars[right_index],list_of_chars[left_index]
+        list_of_chars[left_index],list_of_chars[right_index]=list_of_chars[right_index],list_of_chars[left_index]
         left_index+=1
         right_index-=1
